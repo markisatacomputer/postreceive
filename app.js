b56bf73ae9f1d455ec5c41fbc9e0fa315137aff8
Mark Nicola <marknicola@BWSF-MNicola.local>
2015-08-07 12:40:04 -0700

    first code commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var xhub = require('express-x-hub');
+var config = require('./config');
+
+// Setup server
+var app = express();
+var server = require('http').createServer(app);
+app.use(xhub({ algorithm: 'sha1', secret: prcess.env.GH_SECRET }));
+app.use(bodyParser());
+app.use(methodOverride());
+// Our only route
+app.use('/ghup', require('./githubhook'));
+
+// Start server
+server.listen(80, process.env.IP, function () {
+  console.log('Express server listening on 80');
+});
+
+// Expose app
+exports = module.exports = app;
---
+++ githubhook.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var express = require('express');
+var Q       = require('q');
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  //  Reject if not Xhub
+  if(!req.isXHub) { 
+    return this.reject('No X-Hub Signature', req, res);
+  } else if (!req.isXHubValid()) {
+    return this.reject('Invalid X-Hub Request', req, res);
+  }
+
+  //  move to repo, pull from origin, 
+  runCommands([
+    "cd " + process.env.REPO_DIR,
+    "git pull origin master",
+    "stop " + process.env.PROCESS_NAME,
+    "start " + process.env.PROCESS_NAME
+  ]).then( function(allRes) {
+    res.json(allRes);
+  });
+});
+
+var runCommands = function(commands) {
+  var deferred = Q.defer();
+  if (commands.length > 0) {
+    var command = commands.shift();
+    exec(command, function(error, stdout, stderr) {
+      if (error) {
+        deferred.reject(error);
+      } else {
+        if (commands.length > 0) {
+          runCommands(commands);
+        } else {
+          deferred.resolve(stdout);
+        }
+      }
+    });
+  }
+  
+  return deferred.promise;
+}
+
+module.exports = router;
