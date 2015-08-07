bf6a92c950cf2cb6d0912a8a6557b2b778e6ed56
Mark Nicola <marknicola@BWSF-MNicola.local>
2015-08-07 12:57:43 -0700

    typo fixes

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var xhub = require('express-x-hub');
-var config = require('./config');
 
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
-app.use(xhub({ algorithm: 'sha1', secret: prcess.env.GH_SECRET }));
+app.use(xhub({ algorithm: 'sha1', secret: process.env.GH_SECRET }));
 app.use(bodyParser());
 app.use(methodOverride());
 // Our only route
