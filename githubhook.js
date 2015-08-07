4646080afaaee29d8fcec0bd7e19f8e1c0fca1d2
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-08-07 16:50:51 -0400

    almost there

--- githubhook.js
+++ githubhook.js
@@ -5,6 +5,7 @@ var Q       = require('q');
 var exec = require('child_process').exec,
     child;
 
+
 var router = express.Router();
 
 router.post('/', function(req, res) {
@@ -14,26 +15,28 @@ router.post('/', function(req, res) {
   } else if (!req.isXHubValid()) {
     return this.reject('Invalid X-Hub Request', req, res);
   }
-
-  //  move to repo, pull from origin, 
-  runCommands([
-    "cd " + process.env.REPO_DIR,
-    "git pull origin master",
-    "stop " + process.env.PROCESS_NAME,
-    "start " + process.env.PROCESS_NAME
-  ]).then( function(allRes) {
-    res.json(allRes);
-  });
+      //  restart process 
+      runCommands([
+        "git pull origin master",
+        "stop " + process.env.PROCESS_NAME,
+        "start " + process.env.PROCESS_NAME
+      ]).then( function(allRes) {
+        res.json(allRes);
+      }, function(err){
+        res.status(500).json(err);
+      });
 });
 
 var runCommands = function(commands) {
   var deferred = Q.defer();
   if (commands.length > 0) {
     var command = commands.shift();
-    exec(command, function(error, stdout, stderr) {
+    console.log('executing: ', command);
+    exec(command, {timeout: 15, cwd: process.env.REPO_DIR}, function(error, stdout, stderr) {
       if (error) {
         deferred.reject(error);
       } else {
+        console.log('result: ', stdout);
         if (commands.length > 0) {
           runCommands(commands);
         } else {
